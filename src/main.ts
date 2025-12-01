4c1196bff1b52509e4b9017c239e53575b973159
Joao Bento <16797919+bentovsky@users.noreply.github.com>
2025-12-01 20:53:08 -0300

    feat: add global validation pipe to NestJS application for input validation and transformation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
+
   const configService = app.get<ConfigService>(ConfigService);
   await app.listen(configService.getOrThrow<number>("PORT"));
 }
