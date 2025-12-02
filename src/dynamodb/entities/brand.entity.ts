93f5dc89e47bcd9d714389fce499082ccc4d99fa
Joao Bento <16797919+bentovsky@users.noreply.github.com>
2025-12-02 10:34:09 -0300

    feat: add @Exclude decorator to 'nameLower' attribute in Brand and Location entities for improved serialization control

--- src/dynamodb/entities/brand.entity.ts
+++ src/dynamodb/entities/brand.entity.ts
@@ -1,7 +1,12 @@
+import { Exclude } from "class-transformer";
+
 export class Brand {
   id!: string;
   name!: string;
+
+  @Exclude({ toPlainOnly: true })
   nameLower!: string;
+
   description!: string;
   createdAt!: string;
   updatedAt!: string;
--- src/dynamodb/entities/location.entity.ts
+++ src/dynamodb/entities/location.entity.ts
@@ -1,9 +1,13 @@
-import { Transform } from "class-transformer";
+import { Exclude, Transform } from "class-transformer";
 
 export class Location {
   id!: string;
   brandId!: string;
   name!: string;
+
+  @Exclude({ toPlainOnly: true })
+  nameLower!: string;
+
   address!: string;
 
   @Transform(({ value }) => (value ? Array.from(value) : []), { toPlainOnly: true })
